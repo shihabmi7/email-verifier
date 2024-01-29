c6e325aa356c02632f0054762339c1a6362902ce
Shihab Uddin <shihab.mi7@gmail.com>
2024-01-29 11:42:51 +0800

    Add post request to handle list of emails at a time

--- main.go
+++ main.go
@@ -1,56 +1,62 @@
 package main
 
 import (
- "encoding/json"
- "fmt"
- "net/http"
- emailverifier "github.com/AfterShip/email-verifier"
- "log"
- "github.com/julienschmidt/httprouter"
- "io/ioutil"
- "strings"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"strings"
+
+	emailverifier "github.com/AfterShip/email-verifier"
+	"github.com/julienschmidt/httprouter"
 )
 
 var (
 	verifier = emailverifier.NewVerifier().EnableSMTPCheck()
-	mail = "uddin@emerico.com"
+	mail     = "uddin@emerico.com"
 )
 
-
 type ErrorResponse struct {
-	Code int  `json:"code"` 
-	Status string `json:"status"`
+	Code    int    `json:"code"`
+	Status  string `json:"status"`
 	Message string `json:"message"`
- }  
+}
 
- type SuccesResponse struct {
-	Response interface{} `json:"response"` 
-	Code int  `json:"code"` 
-	Status string `json:"status"`
- }  
+type SuccesResponse struct {
+	Response interface{} `json:"response"`
+	Code     int         `json:"code"`
+	Status   string      `json:"status"`
+}
 
- type EmailListRequest struct {
+type EmailListRequest struct {
 	Emails string `json:"emails"`
 }
 
+type EmailRespose struct {
+	Email         string `json:"email"`
+	IsReachable   string `json:"isReachable"`
+	IsSyntaxValid bool   `json:"isSyntaxValid"`
+}
+
 func ProcessEmailList(w http.ResponseWriter, r *http.Request, _ httprouter.Params) {
-	
+
 	w.Header().Set("Content-Type", "application/json")
 
-	errorResponse := ErrorResponse {
+	errorResponse := ErrorResponse{
 		Code: 400,
-		Status: "Not valid",
+
 		Message: "Error",
-	}	
-	
+	}
+
 	// Read the request body
-	body, err := ioutil.ReadAll(r.Body)
+	body, err := io.ReadAll(r.Body)
 	if err != nil {
 		errorResponse.Message = err.Error()
 		error_json, _ := json.Marshal(errorResponse)
 		//_, _ = fmt.Fprint(w,string(error_json))
 		http.Error(w, string(error_json), http.StatusBadRequest)
-	 
+
 		return
 	}
 
@@ -67,46 +73,70 @@ func ProcessEmailList(w http.ResponseWriter, r *http.Request, _ httprouter.Param
 
 	// Split the comma-separated string into a slice of email addresses
 	emails := strings.Split(request.Emails, ",")
+	responseList := []EmailRespose{}
 
 	// Process the list of emails
 	for _, email := range emails {
 		// Add your logic to validate or process each email address
-		fmt.Println("Email:", email)
+		//fmt.Println("Email:", email)
+		responseList = append(responseList, SimplyVerifyEmail(email))
 	}
 
 	// Respond to the client
 	w.WriteHeader(http.StatusOK)
-	fmt.Fprint(w, "Emails processed successfully")
+	//fmt.Fprint(w, "Emails processed successfully")
+
+	jsonResponse, err := json.Marshal(responseList)
+	fmt.Fprint(w, string(jsonResponse))
+}
+
+func SimplyVerifyEmail(eAddress string) EmailRespose {
+
+	response, err := verifier.Verify(eAddress)
+	if err != nil {
+		return EmailRespose{
+			Email:         eAddress,
+			IsReachable:   "no",
+			IsSyntaxValid: response.Syntax.Valid}
+	}
+
+	if !response.Syntax.Valid {
+		return EmailRespose{
+			Email:         eAddress,
+			IsReachable:   "none",
+			IsSyntaxValid: response.Syntax.Valid}
+	}
+
+	return EmailRespose{Email: eAddress, IsReachable: response.Reachable, IsSyntaxValid: response.Syntax.Valid}
 }
 
 func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	
+
 	w.Header().Set("Content-Type", "application/json")
 
-	errorResponse := ErrorResponse {
-		Code: 400,
-		Status: "Not valid",
+	errorResponse := ErrorResponse{
+		Code:    400,
 		Message: "Error",
-	}	
+	}
 	ret, err := verifier.Verify(ps.ByName("email"))
 
 	if err != nil {
 		errorResponse.Message = err.Error()
 		error_json, _ := json.Marshal(errorResponse)
-		_, _ = fmt.Fprint(w,string(error_json))
+		_, _ = fmt.Fprint(w, string(error_json))
 		return
 	}
 	if !ret.Syntax.Valid {
 		errorResponse.Message = "email address syntax is invalid"
 		error_json, _ := json.Marshal(errorResponse)
-		_, _ = fmt.Fprint(w,string(error_json))
+		_, _ = fmt.Fprint(w, string(error_json))
 		return
 	}
 
-	succesResponse := SuccesResponse {
+	succesResponse := SuccesResponse{
 		Response: ret,
-		Code: 200,
-		Status: "Success",
+		Code:     200,
+		Status:   "Success",
 	}
 
 	jsonResponse, err := json.Marshal(succesResponse)
@@ -117,7 +147,7 @@ func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.
 		error_json, _ := json.Marshal(errorResponse)
 		_, _ = fmt.Fprint(w, string(error_json))
 		return
-	} 
+	}
 
 	_, _ = fmt.Fprint(w, string(jsonResponse))
 
@@ -125,14 +155,10 @@ func GetEmailVerification(w http.ResponseWriter, r *http.Request, ps httprouter.
 
 func main() {
 
-	router:= httprouter.New()
+	router := httprouter.New()
 	router.GET("/v1/:email/verification", GetEmailVerification)
-	router.POST("/process-emails", ProcessEmailList)
-
+	router.POST("/v1/process-emails", ProcessEmailList)
 
 	log.Fatal(http.ListenAndServe(":8041", router))
 
-	
 }
-
-
